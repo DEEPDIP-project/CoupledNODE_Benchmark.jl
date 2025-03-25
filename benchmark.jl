c6becaa17872d23ca39e01c8af2254957296cb4b
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-03-25 15:42:45 +0100

    add plot energy spectra

--- benchmark.jl
+++ benchmark.jl
@@ -88,7 +88,7 @@ end
 
 function _update_ax_limits(ax, x, y)
     # Get data limits with extra space for the legend
-    xmin, xmax = extrema(x) .* (1, 1.5)
+    xmin, xmax = extrema(x)
     ymin, ymax = extrema(y) .* (1, 1.5)
 
     # Get current axis limits
@@ -284,6 +284,131 @@ function plot_energy_evolution(outdir, closure_name, projectorders, params, ax, 
     end
 end
 
+function _get_spectra(setup, u)
+    time_indices = eachindex(u)
+    field_names = [:ref, :nomodel, :cnn_prior, :cnn_post]
+    # Create a nested structure specs[itime][I]
+    specs = map(time_indices) do itime
+        I_indices = eachindex(u[itime].ref)
+        map(I_indices) do I
+            map(field_names) do k
+            state = (; u = u[itime][k][I])
+            spec = observespectrum(state; setup)
+            spec.ehat[]
+            end
+        end
+    end
+    return specs
+end
+
+function plot_energy_spectra(outdir, closure_name, projectorders, params, fig, color)
+    energy_dir = joinpath(outdir, closure_name, "solutions.jld2")
+    solutions = namedtupleload(energy_dir);
+
+    for (igrid, nles) in enumerate(params.nles)
+        setup = getsetup(; params, nles)
+        κ = IncompressibleNavierStokes.spectral_stuff(setup).κ
+        kmax = maximum(κ)
+        all_specs = _get_spectra(setup, solutions.u)
+
+        for (iorder, projectorder) in enumerate(projectorders),
+            (ifil, Φ) in enumerate(params.filters)
+
+            I = CartesianIndex(igrid, ifil, iorder)
+
+            for (itime, t) in enumerate(solutions.t)
+                # Only first time for First
+                projectorder == ProjectOrder.First &&
+                    itime > solutions.itime_max_DIF &&
+                    continue
+                specs = all_specs[itime][I]
+
+                ## Build inertial slope above energy
+                logkrange = [0.45 * log(kmax), 0.85 * log(kmax)]
+                krange = exp.(logkrange)
+                slope, slopelabel = -3.0, L"$\kappa^{-3}$"
+                slopeconst = maximum(specs[1] ./ κ .^ slope)
+                offset = 3
+                inertia = offset .* slopeconst .* krange .^ slope
+
+                ## Nice ticks
+                logmax = round(Int, log2(kmax + 1))
+                if logmax > 5
+                    xticks = [1.0, 4.0, 16.0, 64.0, 256.0]
+                else
+                    xticks = [1.0, 2.0, 4.0, 8.0, 16.0, 32.0]
+                end
+
+                ## Make plot
+                subfig = fig[:, itime]
+                title = "t = $(round(t; digits = 1))"
+                ax = Axis(
+                    subfig;
+                    title = title,
+                    xlabel = "κ",
+                )
+
+                # add No closure
+                lines!(
+                    ax,
+                    κ,
+                    specs[2];
+                    label = "No closure (n = $nles)",
+                    linestyle = :dash,
+                    linewidth = 2,
+                    color = "black",
+                    )
+
+                # add reference
+                lines!(
+                    ax,
+                    κ,
+                    specs[1];
+                    color = "black",
+                    linestyle = :dot,
+                    linewidth = 2,
+                    label = "Reference",
+                )
+
+                label = Φ isa FaceAverage ? "FA" : "VA"
+                lines!(
+                    ax,
+                    κ,
+                    specs[3];
+                    label = "$closure_name (prior) (n = $nles, $label)",
+                    linestyle = :dashdot,
+                    color = color,
+                )
+                lines!(
+                    ax,
+                    κ,
+                    specs[4];
+                    label = "$closure_name (post) (n = $nles, $label)",
+                    color = color,
+                )
+                lines!(
+                    ax,
+                    krange,
+                    inertia;
+                    color = Cycled(1),
+                    label = slopelabel,
+                    linestyle = :dot,
+                )
+                ax.yscale = log10
+                ax.xscale = log2
+
+                if itime == 1
+                    ax.ylabel = "DCF"
+                end
+                if itime == length(solutions.t)
+                    fig[:, itime + 1] = Legend(fig, ax)
+                end
+                # TODO axis limits
+            end
+        end
+    end
+end
+
 # Loop over plot types and configurations
 plot_labels = Dict(
     "prior_error" => Dict(
@@ -306,6 +431,11 @@ plot_labels = Dict(
         "xlabel" => "t",
         "ylabel" => "E(t)",
     ),
+    "energy_spectra" => Dict(
+        "title" => "Energy spectra",
+        "xlabel" => "κ",
+        "ylabel" => "DCF",
+    ),
 )
 
 #TODO check for the colors of internal loops
@@ -316,6 +446,10 @@ for key in keys(plot_labels)
     xlabel = plot_labels[key]["xlabel"]
     ylabel = plot_labels[key]["ylabel"]
     fig, ax = create_figure(title, xlabel, ylabel)
+
+    if key == "energy_spectra"
+        fig = Figure(; size = (950, 600))
+    end
 
     # Loop over the configurations
     for (i, conf_file) in enumerate(list_confs)
@@ -335,11 +469,21 @@ for key in keys(plot_labels)
         elseif key == "energy_evolution"
             projectorders = eval(Meta.parse(conf["posteriori"]["projectorders"]))
             plot_energy_evolution(outdir, closure_name, projectorders, params, ax, color)
+        elseif key=="energy_spectra"
+            Label(
+                fig[0, 0],
+                "Energy spectra for different configurations",
 
+            )
+            subfig = fig[i, :]
+            projectorders = eval(Meta.parse(conf["posteriori"]["projectorders"]))
+            plot_energy_spectra(outdir, closure_name, projectorders, params, subfig, color)
         end
     end
     # Add legend
-    axislegend(ax)
+    if key != "energy_spectra"
+        axislegend(ax)
+    end
 
     # Display and save the figure
     save("$compdir/$(key).pdf", fig)
