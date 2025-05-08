ff2269a6b8d09765a0c70b9908ee36cce62ecec4
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-05-08 10:29:44 +0200

    add plotting parameters count

--- src/Benchmark.jl
+++ src/Benchmark.jl
@@ -101,7 +101,7 @@ export read_config, check_necessary_files
 export _convert_to_single_index,
     plot_prior_traininghistory, plot_posteriori_traininghistory,
     plot_divergence, plot_energy_evolution, plot_energy_spectra,
-    plot_prior_time, plot_posteriori_time
+    plot_prior_time, plot_posteriori_time, plot_num_parameters
 
 export compute_eprior, compute_epost
 
