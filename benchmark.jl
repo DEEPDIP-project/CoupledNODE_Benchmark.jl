d42a231ed548f9da138ac024bff147b4d53bcfeb
SCiarella <simoneciarella@gmail.com>
2025-06-12 09:42:00 +0200

    add extra confs

--- benchmark.jl
+++ benchmark.jl
@@ -18,7 +18,8 @@ ispath(compdir) || mkpath(compdir)
 
 # List configurations files
 using Glob
-exclude_patterns = ["att", "cno", "cnn_ins", "_1"]
+#exclude_patterns = ["att", "cno", "cnn_ins", "_1", "nopr"]
+exclude_patterns = ["att", "cno", "int", "back", "rk4"]
 @warn "Excluding configurations with patterns: $(exclude_patterns)"
 all_confs = glob("*.yaml", confdir)
 list_confs = filter(conf -> all(!occursin(pat, conf) for pat in exclude_patterns), all_confs)
--- multisub.sh
+++ multisub.sh
@@ -1,3 +1,7 @@
 #sbatch -J CNO job_a100_extra.sh configs/snellius/cno_1.yaml
 sbatch -J ATT job_a100_extra.sh configs/snellius/att_1.yaml
 sbatch -J CNN job_a100.sh configs/snellius/cnn_1.yaml
+
+sbatch -J backsol job_a100.sh configs/snellius/cnn_backsol.yaml
+sbatch -J interp job_a100.sh configs/snellius/cnn_interp.yaml
+sbatch -J rk4 job_a100.sh configs/snellius/cnn_rk4.yaml
